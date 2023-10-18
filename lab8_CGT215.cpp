e4a01eed56c6f37f2c73ae07235b8b5806624719
Griffin Overmyer <griffinovermyer@gmail.com>
2023-10-18 11:48:09 -0400

    score updating and shooting arrows

--- lab8_CGT215.cpp
+++ lab8_CGT215.cpp
@@ -10,35 +10,26 @@ using namespace std;
 using namespace sf;
 using namespace sfp;
 
-const float KB_SPEED = 0.2;
-
 void LoadTex(Texture& tex, string filename) {
     if (!tex.loadFromFile(filename)) {
         cout << "Could not load " << filename << endl;
     }
 }
 
-void MoveCrossbow(PhysicsSprite& crossbow, int elapsedMS) {
-    if (Keyboard::isKeyPressed(Keyboard::Right)) {
-        Vector2f newPos(crossbow.getCenter());
-        newPos.x = newPos.x + (KB_SPEED * elapsedMS);
-        crossbow.setCenter(newPos);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Left)) {
-        Vector2f newPos(crossbow.getCenter());
-        newPos.x = newPos.x - (KB_SPEED * elapsedMS);
-        crossbow.setCenter(newPos);
-    }
-}
-
 int main()
 {
-    RenderWindow window(VideoMode(800, 600), "Balloon Buster");
+    RenderWindow window(VideoMode(800, 600), "Duck Hunter");
     World world(Vector2f(0, 0));
     int score(0);
     int arrows(5);
+    int fontSz(48);
+    Font font;
+    if (!font.loadFromFile("Montserrat.ttf")) {
+        cout << "could not load Montserrat.ttf" << endl;
+        exit(0);
+    }
 
-    PhysicsSprite& crossBow = *new PhysicsSprite();
+    PhysicsSprite crossBow;
     Texture cbowTex;
     LoadTex(cbowTex, "images/crossbow.png");
     crossBow.setTexture(cbowTex);
@@ -54,116 +45,53 @@ int main()
 
     PhysicsRectangle top;
     top.setSize(Vector2f(800, 10));
-    top.setCenter(Vector2f(400, 5));
+    top.setCenter(Vector2f(400, -5));
     top.setStatic(true);
-    world.AddPhysicsBody(top);
-
-    PhysicsRectangle left;
-    left.setSize(Vector2f(10, 600));
-    left.setCenter(Vector2f(5, 300));
-    left.setStatic(true);
-    world.AddPhysicsBody(left);
-
-    PhysicsRectangle right;
-    right.setSize(Vector2f(10, 600));
-    right.setCenter(Vector2f(795, 300));
-    right.setStatic(true);
-    world.AddPhysicsBody(right);
-
-    Texture redTex;
-    LoadTex(redTex, "images/red_balloon.png");
-    PhysicsShapeList<PhysicsSprite> balloons;
-    for (int i(0); i < 6; i++) {
-        PhysicsSprite& balloon = balloons.Create();
-        balloon.setTexture(redTex);
-        int x = 50 + ((700 / 5) * i);
-        Vector2f sz = balloon.getSize();
-        balloon.setCenter(Vector2f(x, 20 + (sz.y / 2)));
-        balloon.setVelocity(Vector2f(0.25, 0));
-        world.AddPhysicsBody(balloon);
-        balloon.onCollision =
-            [&drawingArrow, &world, &arrow, &balloon, &balloons, &score]
-            (PhysicsBodyCollisionResult result) {
-            if (result.object2 == arrow) {
-                drawingArrow = false;
-                world.RemovePhysicsBody(arrow);
-                world.RemovePhysicsBody(balloon);
-                balloons.QueueRemove(balloon);
-                score += 10;
-            }
-            };
-    }
-
-    top.onCollision = [&drawingArrow, &world, &arrow]
-    (PhysicsBodyCollisionResult result) {
+    top.onCollision = [&arrow, &arrows, &world, &drawingArrow](PhysicsBodyCollisionResult res) {
+        arrows--;
         drawingArrow = false;
         world.RemovePhysicsBody(arrow);
-        };
+    };
+    world.AddPhysicsBody(top);
 
-    Text scoreText;
-    Font font;
-    if (!font.loadFromFile("Montserrat.ttf")) {
-        cout << "Couldn't load font arial.ttf" << endl;
-        exit(1);
-    }
-    scoreText.setFont(font);
-    Text arrowCountText;
-    arrowCountText.setFont(font);
+    Text arrowText;
+    arrowText.setFont(font);
+    arrowText.setCharacterSize(fontSz);
 
     Clock clock;
     Time lastTime(clock.getElapsedTime());
-    Time currentTime(lastTime);
+    while (arrows > 0) {
+        Time currentTime(clock.getElapsedTime());
+        int deltaTime((currentTime - lastTime).asMilliseconds());
 
-    while ((arrows > 0) || drawingArrow) {
-        currentTime = clock.getElapsedTime();
-        Time deltaTime = currentTime - lastTime;
-        long deltaMS = deltaTime.asMilliseconds();
-        if (deltaMS > 9) {
+        if (deltaTime > 2) {
             lastTime = currentTime;
-            world.UpdatePhysics(deltaMS);
-            MoveCrossbow(crossBow, deltaMS);
-            if (Keyboard::isKeyPressed(Keyboard::Space) &&
-                !drawingArrow) {
+            world.UpdatePhysics(deltaTime);
+
+            if (Keyboard::isKeyPressed(Keyboard::Space) && !drawingArrow) {
                 drawingArrow = true;
                 arrow.setCenter(crossBow.getCenter());
-                arrow.setVelocity(Vector2f(0, -1));
+                arrow.setVelocity(Vector2f(0, -1.2));
                 world.AddPhysicsBody(arrow);
-                arrows -= 1;
             }
 
             window.clear();
+            window.draw(crossBow);
             if (drawingArrow) {
                 window.draw(arrow);
             }
-            for (PhysicsShape& balloon : balloons) {
-                window.draw((PhysicsSprite&)balloon);
-            }
-            window.draw(crossBow);
-            scoreText.setString(to_string(score));
-            FloatRect textBounds = scoreText.getGlobalBounds();
-            scoreText.setPosition(
-                Vector2f(790 - textBounds.width, 590 - textBounds.height));
-            window.draw(scoreText);
-            arrowCountText.setString(to_string(arrows));
-            textBounds = arrowCountText.getGlobalBounds();
-            arrowCountText.setPosition(
-                Vector2f(10, 590 - textBounds.height));
-            window.draw(arrowCountText);
-            //world.VisualizeAllBounds(window);
+            //arrows text
+            arrowText.setString(to_string(arrows));
+            FloatRect arrowSz(arrowText.getGlobalBounds());
+            arrowText.setPosition(Vector2f(20 - (arrowSz.width / 2), 560 - (arrowSz.height / 2)));
+            window.draw(arrowText);
 
             window.display();
-            balloons.DoRemovals();
+            
         }
+
+
     }
-    Text gameOverText;
-    gameOverText.setFont(font);
-    gameOverText.setString("GAME OVER");
-    FloatRect textBounds = gameOverText.getGlobalBounds();
-    gameOverText.setPosition(Vector2f(
-        400 - (textBounds.width / 2),
-        300 - (textBounds.height / 2)
-    ));
-    window.draw(gameOverText);
-    window.display();
-    while (true);
+
+
 }
