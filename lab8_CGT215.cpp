2efb4380ede526f0751d06b5056093ff051e139a
Griffin Overmyer <griffinovermyer@gmail.com>
2023-10-18 18:25:59 -0400

    working duck hunter game with end score card

--- lab8_CGT215.cpp
+++ lab8_CGT215.cpp
@@ -62,11 +62,8 @@ int main()
     //duck wall
     PhysicsRectangle rightWall;
     rightWall.setSize(Vector2f(10, 600));
-    rightWall.setCenter(Vector2f(605, 400)); //just over the right side of the viewport
+    rightWall.setCenter(Vector2f(950, 400)); //just over the right side of the viewport
     rightWall.setStatic(true);
-    rightWall.onCollision = [](PhysicsBodyCollisionResult res) {
-        //remove duck from list
-        };
     world.AddPhysicsBody(rightWall);
 
 
@@ -125,7 +122,7 @@ int main()
                 newDuck.setCenter(Vector2f(0, 120 - (newDuck.getGlobalBounds().height / 2)));
                 newDuck.setVelocity(Vector2f(0.5, 0));
                 world.AddPhysicsBody(newDuck);
-                newDuck.onCollision = [&world, &arrow, &arrows, &drawingArrow, &newDuck, &ducks, &score](PhysicsBodyCollisionResult res) {
+                newDuck.onCollision = [&world, &arrow, &arrows, &drawingArrow, &newDuck, &ducks, &score, &rightWall](PhysicsBodyCollisionResult res) {
                     if (res.object2 == arrow) {
                         drawingArrow = false;
                         arrows--;
@@ -134,17 +131,67 @@ int main()
                         ducks.QueueRemove(newDuck);
                         score += 10;
                     }
+                    if (res.object2 == rightWall) {
+                        world.RemovePhysicsBody(newDuck);
+                        ducks.QueueRemove(newDuck);
+                    }
+                    
                 };
             }
             for (auto duck : ducks) {
                 window.draw(duck);
             }
+            window.draw(rightWall); //wall to remove ducks from ducks
             
             window.display();
             ducks.DoRemovals();
         }
 
 
+    }
+
+    //end game screen
+    bool exitGame(false);
+    PhysicsRectangle backboard;
+    backboard.setSize(Vector2f(500, 300));
+    backboard.setCenter(Vector2f(400, 300));
+    backboard.setFillColor(Color(240, 240, 240));
+    backboard.setStatic(true);
+
+    Text gameOverText;
+    gameOverText.setFont(font);
+    gameOverText.setString("Game Over");
+    gameOverText.setCharacterSize(fontSz);
+    gameOverText.setFillColor(Color(20, 200, 240));
+    FloatRect goSz = gameOverText.getGlobalBounds();
+    gameOverText.setPosition(Vector2f(400 - (goSz.width / 2), 200 - (goSz.height)));
+
+    Text endScoreText;
+    endScoreText.setFont(font);
+    endScoreText.setString("You scored: " + to_string(score) + " points!");
+    endScoreText.setCharacterSize(fontSz);
+    endScoreText.setFillColor(Color(20, 200, 240));
+    FloatRect estSz = endScoreText.getGlobalBounds();
+    endScoreText.setPosition(Vector2f(400 - (estSz.width / 2), 300 - (estSz.height)));
+
+    Text leaveText;
+    leaveText.setFont(font);
+    leaveText.setString("Press SPACE to exit game");
+    leaveText.setCharacterSize(fontSz * 0.75);
+    leaveText.setFillColor(Color(200, 165, 240));
+    FloatRect lSz = leaveText.getGlobalBounds();
+    leaveText.setPosition(Vector2f(400 - (lSz.width / 2), 400 - (lSz.height)));
+
+    while (!exitGame) {
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            exitGame = true;
+        }
+        window.clear();
+        window.draw(backboard);
+        window.draw(gameOverText);
+        window.draw(endScoreText);
+        window.draw(leaveText);
+        window.display();
     }
 
 
